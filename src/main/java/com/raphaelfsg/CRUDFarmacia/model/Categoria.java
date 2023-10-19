7e5f5d24141df2836349f9e43ab7948acdb61307
raphaelfsg <raphaelfsg@gmail.com>
2023-10-19 10:36:18 -0300

    CRUD-Categoria

---
+++ src/main/java/com/raphaelfsg/CRUDFarmacia/model/Categoria.java
@@ -0,0 +1,43 @@
+package com.raphaelfsg.CRUDFarmacia.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O atributo nome da categoria é obrigatório!")
+    private String nomeCategoria;
+
+    @NotNull(message = "O atributo descrição é obrigatório!")
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
